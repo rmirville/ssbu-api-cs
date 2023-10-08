1db9270e3df9a6d9e4f85aebdad4dd49b5a8a177
Raynald Mirville <raynaldm@pm.me>
2023-10-08 19:09:06 -0400

    add missing gamedata fields

--- src/SsbuTools/Api/Models/Stage/GameData/StageGameData.cs
+++ src/SsbuTools/Api/Models/Stage/GameData/StageGameData.cs
@@ -14,34 +14,53 @@ public class StageGameData : IStageGameData
 	public string Lvd { get; set; }
 
 	[BsonElement("collisions")]
-	public List<object> Collisions { get; set; }
+	public List<object> Collisions { get; set; } = new List<object>();
 
 	[BsonElement("platforms")]
-	public List<object> Platforms	{ get; set; }
+	public List<object> Platforms	{ get; set; } = new List<object>();
 
 	[BsonElement("blast_zones")]
-	public List<object> BlastZones { get; set; }
+	public List<object> BlastZones { get; set; } = new List<object>();
 
 	[BsonElement("camera")]
-	public List<object> Camera { get; set; }
+	public List<object> Camera { get; set; } = new List<object>();
 
 	[BsonElement("center")]
-	public List<object> Center { get; set; }
+	public List<object> Center { get; set; } = new List<object>();
 
 	[BsonElement("spawns")]
-	public List<object> Spawns { get; set; }
+	public List<object> Spawns { get; set; } = new List<object>();
 
 	[BsonElement("respawns")]
-	public List<object> Respawns { get; set; }
+	public List<object> Respawns { get; set; } = new List<object>();
 
 	[BsonElement("items")]
-	public List<object> Items	{ get; set; }
+	public List<object> Items	{ get; set; } = new List<object>();
 
-	public StageGameData(string stage, string name, string lvd)
+	public StageGameData(
+		string stage,
+		string name,
+		string lvd, List<object> collisions,
+		List<object> platforms,
+		List<object> blastZones,
+		List<object> camera,
+		List<object> center,
+		List<object> spawns,
+		List<object> respawns,
+		List<object> items
+	)
 	{
 		Stage = stage;
 		Name = name;
 		Lvd = lvd;
+		Collisions = collisions;
+		Platforms = platforms;
+		BlastZones = blastZones;
+		Camera = camera;
+		Center = center;
+		Spawns = spawns;
+		Respawns = respawns;
+		Items = items;
 	}
 
 	public StageGameData(IStageGameData data)
@@ -49,5 +68,13 @@ public class StageGameData : IStageGameData
 		Stage = data.Stage;
 		Name = data.Name;
 		Lvd = data.Lvd;
+		Collisions = data.Collisions;
+		Platforms = data.Platforms;
+		BlastZones = data.BlastZones;
+		Camera = data.Camera;
+		Center = data.Center;
+		Spawns = data.Spawns;
+		Respawns = data.Respawns;
+		Items = data.Items;
 	}
 }
