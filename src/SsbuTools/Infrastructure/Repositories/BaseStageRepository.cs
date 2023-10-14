e06a7d73b0fcc4d3e0188127588c0433a4ee75c6
Raynald Mirville <raynaldm@pm.me>
2023-10-14 08:25:57 -0400

    add base getall, getbyid repository methods

--- src/SsbuTools/Infrastructure/Repositories/BaseStageRepository.cs
+++ src/SsbuTools/Infrastructure/Repositories/BaseStageRepository.cs
@@ -1,18 +1,19 @@
 using Microsoft.Extensions.Options;
 using MongoDB.Bson.Serialization.Conventions;
 using MongoDB.Driver;
+using SsbuTools.Api.Entities;
 using SsbuTools.Api.Options;
 
 namespace SsbuTools.Infrastructure.Repositories;
 
-public abstract class BaseStageRepository<T>
+public abstract class BaseStageRepository<TEntity> where TEntity : MongoEntity
 {
 	protected MongoOptions MongoConfig { get; init; }
 	protected IMongoDatabase Database { get; init; }
 
 	protected abstract string CollectionName { get; }
 
-	protected IMongoCollection<T>? Collection { get; init; }
+	protected IMongoCollection<TEntity>? Collection { get; init; }
 
 	public BaseStageRepository(IOptions<MongoOptions> dbConfigOptions)
 	{
@@ -27,6 +28,16 @@ public abstract class BaseStageRepository<T>
 		// This allows automapping of the camelCase database fields to our models. 
 		var camelCaseConvention = new ConventionPack { new CamelCaseElementNameConvention() };
 		ConventionRegistry.Register("CamelCase", camelCaseConvention, type => true);
-		Collection = Database.GetCollection<T>(CollectionName);
+		Collection = Database.GetCollection<TEntity>(CollectionName);
+	}
+
+	public async Task<List<TEntity>> GetAllAsync()
+	{
+		return await Collection.Find(FilterDefinition<TEntity>.Empty).ToListAsync();
+	}
+
+	public async Task<TEntity> GetAsync(string id)
+	{
+		return await Collection.Find(set => set.Id == id).FirstOrDefaultAsync();
 	}
 }
