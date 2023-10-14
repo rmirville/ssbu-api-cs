3f45f2a8bffc0d48ac98539b52437c2af9fb8107
Raynald Mirville <raynaldm@pm.me>
2023-10-14 07:25:05 -0400

    derive classifications repository from base

--- src/SsbuTools/Infrastructure/Repositories/StageClassificationsRepository.cs
+++ src/SsbuTools/Infrastructure/Repositories/StageClassificationsRepository.cs
@@ -6,42 +6,26 @@ using SsbuTools.Api.Entities;
 
 namespace SsbuTools.Infrastructure.Repositories;
 
-public class StageClassificationsRepository
+public class StageClassificationsRepository : SsbuToolsRepository<StageClassificationsEntity>
 {
-	private readonly MongoOptions _mongoConfig;
-
-	private static IMongoCollection<StageClassificationsEntity>? _stageClassificationsCollection;
-
-	public StageClassificationsRepository(IOptions<MongoOptions> dbConfigOptions)
+	public StageClassificationsRepository(IOptions<MongoOptions> dbConfigOptions) : base(dbConfigOptions)
 	{
-		_mongoConfig = dbConfigOptions.Value;
-		var settings = MongoClientSettings.FromConnectionString(_mongoConfig.Url);
-		settings.ServerApi = new ServerApi(ServerApiVersion.V1);
-
-		// Establish the connection to MongoDB
-		var client = new MongoClient(settings);
-		var database = client.GetDatabase(_mongoConfig.DatabaseName);
-
-		_stageClassificationsCollection = database.GetCollection<StageClassificationsEntity>("stage_classifications");
-
-		// This allows automapping of the camelCase database fields to our models. 
-		var camelCaseConvention = new ConventionPack { new CamelCaseElementNameConvention() };
-		ConventionRegistry.Register("CamelCase", camelCaseConvention, type => true);
+		_collection = _database.GetCollection<StageClassificationsEntity>("stage_classifications");
 	}
 
 	public async Task<List<StageClassificationsEntity>> GetAllStagesAsync()
 	{
-		return await _stageClassificationsCollection.Find(FilterDefinition<StageClassificationsEntity>.Empty)
+		return await _collection.Find(FilterDefinition<StageClassificationsEntity>.Empty)
 		.ToListAsync();
 	}
 
 	public async Task<StageClassificationsEntity> GetStageByIdAsync(string id)
 	{
-		return await _stageClassificationsCollection.Find(stage => stage.Id == id).FirstOrDefaultAsync();
+		return await _collection.Find(stage => stage.Id == id).FirstOrDefaultAsync();
 	}
 
 	public async Task<List<StageClassificationsEntity>> GetStagesByIdsAsync(string[] ids)
 	{
-		return await _stageClassificationsCollection.Find(stage => ids.Contains(stage.Id)).ToListAsync();
+		return await _collection.Find(stage => ids.Contains(stage.Id)).ToListAsync();
 	}
 }
