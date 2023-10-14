b12ff884ce28c0a7b81d5d9f9363fcef4225b8c3
Raynald Mirville <raynaldm@pm.me>
2023-10-14 07:48:29 -0400

    address nullability warnings

--- src/SsbuTools/Api/Options/MongoOptions.cs
+++ src/SsbuTools/Api/Options/MongoOptions.cs
@@ -3,6 +3,6 @@ namespace SsbuTools.Api.Options;
 public sealed class MongoOptions
 {
 	public const string ConfigName = "SsbuTools:Db:Mongo";
-	public string Url { get; set; }
-	public string DatabaseName { get; set; }
+	public string? Url { get; set; }
+	public string? DatabaseName { get; set; }
 }
