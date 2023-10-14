3a8db2602c9d2966f95e1e3f510d481b4130bf4d
Raynald Mirville <raynaldm@pm.me>
2023-10-14 07:33:23 -0400

    derive set repository from base

--- src/SsbuTools/Infrastructure/Repositories/StageSetRepository.cs
+++ src/SsbuTools/Infrastructure/Repositories/StageSetRepository.cs
@@ -6,38 +6,21 @@ using SsbuTools.Api.Entities;
 
 namespace SsbuTools.Infrastructure.Repositories;
 
-public class StageSetRepository
+public class StageSetRepository : SsbuToolsRepository<StageSetEntity>
 {
-	private readonly MongoOptions _mongoConfig;
-
-	private static IMongoCollection<StageSetEntity>? _stageSetCollection;
-
-	public StageSetRepository(IOptions<MongoOptions> dbConfigOptions)
+	public StageSetRepository(IOptions<MongoOptions> dbConfigOptions) : base(dbConfigOptions)
 	{
-		_mongoConfig = dbConfigOptions.Value;
-		var settings = MongoClientSettings.FromConnectionString(_mongoConfig.Url);
-		settings.ServerApi = new ServerApi(ServerApiVersion.V1);
-
-		// Establish the connection to MongoDB
-		var client = new MongoClient(settings);
-		var database = client.GetDatabase(_mongoConfig.DatabaseName);
-
-		_stageSetCollection = database.GetCollection<StageSetEntity>("stage_sets");
-
-		// This allows automapping of the camelCase database fields to our models. 
-		var camelCaseConvention = new ConventionPack { new CamelCaseElementNameConvention() };
-		ConventionRegistry.Register("CamelCase", camelCaseConvention, type => true);
+		_collection = _database.GetCollection<StageSetEntity>("stage_sets");
 	}
 
 	public async Task<List<StageSetEntity>> GetAllStageSetsAsync()
 	{
-		return await _stageSetCollection.Find(FilterDefinition<StageSetEntity>.Empty)
+		return await _collection.Find(FilterDefinition<StageSetEntity>.Empty)
 		.ToListAsync();
 	}
 
 	public async Task<StageSetEntity> GetStageSetByIdAsync(string id)
 	{
-		return await _stageSetCollection.Find(set => set.Id == id).FirstOrDefaultAsync();
+		return await _collection.Find(set => set.Id == id).FirstOrDefaultAsync();
 	}
-
 }
