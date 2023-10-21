827175f7ccf9364f240ff317deed398e6144ad49
Raynald Mirville <raynaldm@pm.me>
2023-10-21 19:40:09 -0400

    create StageSummaryCollectionResponse

---
+++ src/SsbuTools/Api/Responses/Stage/StageSummaryCollectionResponse.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using SsbuTools.Api.Dtos.Resource;
+using SsbuTools.Api.Dtos.Stage;
+
+namespace SsbuTools.Api.Responses;
+
+public class StageSummaryCollectionResponse : SsbuToolsResponse
+{
+	private readonly JsonResult _result;
+	public StageSummaryCollectionResponse(List<IStageClassifications> collection, string baseStagesUrl)
+	{
+		var summaries = collection.Select(stage =>
+		{
+			var links = new Dictionary<string, string> {
+				{
+					"self", $"{baseStagesUrl}/{stage.Id}"
+				}
+			};
+			return new RestResource<StageSummary>(links, new StageSummary(stage));
+		}).ToArray();
+		
+		var embed = new StageSummariesEmbed(summaries);
+
+		var links = new Dictionary<string, string> {
+			{ "self", baseStagesUrl },
+			{ "classifications", $"{baseStagesUrl}/classifications" },
+			{ "classificationSets", $"{baseStagesUrl}/classification-sets" },
+			{ "gameData", $"{baseStagesUrl}/game-data" },
+			{ "pieceMaps", $"{baseStagesUrl}/piece-maps" },
+		};
+		
+		_result = new JsonResult(new RestResource<StageSummariesEmbed>(links, embed));
+	}
+
+	public override JsonResult ToJsonResult() => _result;
+}
